35b1c66153268a762ce96cd902fdd67ea2f57da7
varun <varunsvga@gmail.com>
2025-09-12 09:04:06 +0530

    addded dataset generation

--- proposedSolution/utils.py
+++ proposedSolution/utils.py
@@ -1,1 +1,8 @@
 # Utilities Implementation
+def parse_variables(file_path):
+    variables = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            key, value = line.strip().split('=', 1)
+            variables[key] = ast.literal_eval(value)
+    return variables
